de5e63348b66274dbd3604f43f58dcff8aaf3e89
Helen0105 <marinenkoolena21@gmail.com>
2026-05-09 22:06:30 +0300

    2

--- script.js
+++ script.js
@@ -11,43 +11,105 @@
 
 
 
-// let minutesLeft = 3;
-// const minutesDisplay = document.querySelector(".timer"); 
+// // let minutesLeft = 3;
+// // const minutesDisplay = document.querySelector(".timer"); 
 
-// const minutesTimer = setInterval(() => {
-//     minutesLeft--;
+// // const minutesTimer = setInterval(() => {
+// //     minutesLeft--;
 
     
-//     if (minutesDisplay) {
-//         minutesDisplay.textContent = `Залишилось ${minutesLeft} хвилини`;
+// //     if (minutesDisplay) {
+// //         minutesDisplay.textContent = `Залишилось ${minutesLeft} хвилини`;
+// //     }
+
+// //     if (minutesLeft === 1) {
+// //         alert("Залишилася 1 хвилина");
+// //     }
+
+// // }, 60000); 
+
+
+// let secondsLeft = 30;
+// const secondsDisplay = document.querySelector(".timer2");
+
+// const updateDisplay = (time) => {
+//     if (secondsDisplay) {
+//         secondsDisplay.textContent = `Залишилося ${time} секунд`;
+//     }
+// };
+
+
+// updateDisplay(secondsLeft);
+
+// const secondsTimer = setInterval(() => {
+//     secondsLeft--;
+//     updateDisplay(secondsLeft);
+
+//     if (secondsLeft === 10) {
+//         alert("Залишилося 10 секунд");
 //     }
 
-//     if (minutesLeft === 1) {
-//         alert("Залишилася 1 хвилина");
-//     }
+//     if (secondsLeft <= 0) {
 
-// }, 60000); 
+//         clearInterval(secondsTimer);
+//         alert("Час вийшов!!!!!!!");
+//     }
+// }, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+let minutesLeft = 3;
+const minutesDisplay = document.querySelector(".timer");
+
+const minutesTimer = setInterval(() => {
+    minutesLeft--;
+    if (minutesDisplay) {
+        minutesDisplay.textContent = `Залишилося: ${minutesLeft} хвилини`;
+    }
+
+    if (minutesLeft === 2) {
+        alert("Залишилося 2 хвилини");
+    }
+
+    if (minutesLeft <= 0) {
+        clearInterval(minutesTimer);
+        alert("Час першого таймера вийшов!");
+    }
+}, 60000);
+
+
+
+
 
 
 let secondsLeft = 30;
 const secondsDisplay = document.querySelector(".timer2");
 
+const updateSecondsDisplay = (time) => {
+    if (secondsDisplay) {
+        secondsDisplay.textContent = `Залишилося: ${time} секунд`;
+    }
+};
+
 const secondsTimer = setInterval(() => {
     secondsLeft--;
-
-    
-    if (secondsDisplay) {
-        secondsDisplay.textContent = `Залишилось ${secondsLeft} секунд`;
-    }
+    updateSecondsDisplay(secondsLeft);
 
     if (secondsLeft === 10) {
-        alert("Залишилася 10 секунд");
+        alert("Залишилося 10 секунд");
     }
 
-    if (secondsLeft === 0) {
-        alert("час вийшов!!!!!!!");
+    if (secondsLeft <= 0) {
+        clearInterval(secondsTimer);
+        alert("Час другого таймеру вийшов!!");
     }
-
-    
-
-}, 1000); 
+}, 1000);
